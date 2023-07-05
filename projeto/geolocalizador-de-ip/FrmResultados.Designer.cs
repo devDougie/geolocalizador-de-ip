a6b0933581ab933469e51ea737fc19b7d2820db3
Douglas Venancio <99559030+devDougie@users.noreply.github.com>
2023-07-05 16:44:07 -0300

    Project Update

---
+++ projeto/geolocalizador-de-ip/FrmResultados.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace geolocalizador_de_ip
+{
+    partial class FrmResultados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmResultados));
+            lblConexao = new Label();
+            lblLocalizacao = new Label();
+            lblHorario = new Label();
+            lblHost = new Label();
+            lblProvedor = new Label();
+            lblPais = new Label();
+            lblEstado = new Label();
+            lblCidade = new Label();
+            lblContinente = new Label();
+            lblCodigoPostal = new Label();
+            lblLatitude = new Label();
+            lblLongitude = new Label();
+            lblMoeda = new Label();
+            lblFusoHorario = new Label();
+            txbHost = new TextBox();
+            txbProvedor = new TextBox();
+            txbPais = new TextBox();
+            txbEstado = new TextBox();
+            txbCidade = new TextBox();
+            txbContinente = new TextBox();
+            txbCodigoPostal = new TextBox();
+            txbLatitude = new TextBox();
+            txbLongitude = new TextBox();
+            txbMoeda = new TextBox();
+            txbFusoHorario = new TextBox();
+            lblOBS = new Label();
+            SuspendLayout();
+            // 
+            // lblConexao
+            // 
+            lblConexao.AutoSize = true;
+            lblConexao.BackColor = Color.Red;
+            lblConexao.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblConexao.ForeColor = Color.White;
+            lblConexao.Location = new Point(14, 29);
+            lblConexao.Name = "lblConexao";
+            lblConexao.Size = new Size(71, 17);
+            lblConexao.TabIndex = 0;
+            lblConexao.Text = "CONEXÃO";
+            // 
+            // lblLocalizacao
+            // 
+            lblLocalizacao.AutoSize = true;
+            lblLocalizacao.BackColor = Color.Red;
+            lblLocalizacao.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblLocalizacao.ForeColor = Color.White;
+            lblLocalizacao.Location = new Point(14, 152);
+            lblLocalizacao.Name = "lblLocalizacao";
+            lblLocalizacao.Size = new Size(97, 17);
+            lblLocalizacao.TabIndex = 1;
+            lblLocalizacao.Text = "LOCALIZAÇÃO";
+            // 
+            // lblHorario
+            // 
+            lblHorario.AutoSize = true;
+            lblHorario.BackColor = Color.Red;
+            lblHorario.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblHorario.ForeColor = Color.White;
+            lblHorario.Location = new Point(15, 484);
+            lblHorario.Name = "lblHorario";
+            lblHorario.Size = new Size(60, 17);
+            lblHorario.TabIndex = 2;
+            lblHorario.Text = "OUTROS";
+            // 
+            // lblHost
+            // 
+            lblHost.AutoSize = true;
+            lblHost.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblHost.Location = new Point(14, 59);
+            lblHost.Name = "lblHost";
+            lblHost.Size = new Size(41, 17);
+            lblHost.TabIndex = 4;
+            lblHost.Text = "Host:";
+            // 
+            // lblProvedor
+            // 
+            lblProvedor.AutoSize = true;
+            lblProvedor.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblProvedor.Location = new Point(14, 90);
+            lblProvedor.Name = "lblProvedor";
+            lblProvedor.Size = new Size(69, 17);
+            lblProvedor.TabIndex = 5;
+            lblProvedor.Text = "Provedor:";
+            // 
+            // lblPais
+            // 
+            lblPais.AutoSize = true;
+            lblPais.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPais.Location = new Point(13, 183);
+            lblPais.Name = "lblPais";
+            lblPais.Size = new Size(37, 17);
+            lblPais.TabIndex = 6;
+            lblPais.Text = "País:";
+            // 
+            // lblEstado
+            // 
+            lblEstado.AutoSize = true;
+            lblEstado.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblEstado.Location = new Point(14, 214);
+            lblEstado.Name = "lblEstado";
+            lblEstado.Size = new Size(53, 17);
+            lblEstado.TabIndex = 7;
+            lblEstado.Text = "Estado:";
+            // 
+            // lblCidade
+            // 
+            lblCidade.AutoSize = true;
+            lblCidade.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCidade.Location = new Point(14, 245);
+            lblCidade.Name = "lblCidade";
+            lblCidade.Size = new Size(54, 17);
+            lblCidade.TabIndex = 8;
+            lblCidade.Text = "Cidade:";
+            // 
+            // lblContinente
+            // 
+            lblContinente.AutoSize = true;
+            lblContinente.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblContinente.Location = new Point(14, 276);
+            lblContinente.Name = "lblContinente";
+            lblContinente.Size = new Size(80, 17);
+            lblContinente.TabIndex = 9;
+            lblContinente.Text = "Continente:";
+            // 
+            // lblCodigoPostal
+            // 
+            lblCodigoPostal.AutoSize = true;
+            lblCodigoPostal.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCodigoPostal.Location = new Point(15, 360);
+            lblCodigoPostal.Name = "lblCodigoPostal";
+            lblCodigoPostal.Size = new Size(153, 17);
+            lblCodigoPostal.TabIndex = 10;
+            lblCodigoPostal.Text = "Faixa de Código Postal:";
+            // 
+            // lblLatitude
+            // 
+            lblLatitude.AutoSize = true;
+            lblLatitude.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblLatitude.Location = new Point(15, 391);
+            lblLatitude.Name = "lblLatitude";
+            lblLatitude.Size = new Size(63, 17);
+            lblLatitude.TabIndex = 11;
+            lblLatitude.Text = "Latitude:";
+            // 
+            // lblLongitude
+            // 
+            lblLongitude.AutoSize = true;
+            lblLongitude.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblLongitude.Location = new Point(15, 422);
+            lblLongitude.Name = "lblLongitude";
+            lblLongitude.Size = new Size(75, 17);
+            lblLongitude.TabIndex = 12;
+            lblLongitude.Text = "Longitude:";
+            // 
+            // lblMoeda
+            // 
+            lblMoeda.AutoSize = true;
+            lblMoeda.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblMoeda.Location = new Point(14, 515);
+            lblMoeda.Name = "lblMoeda";
+            lblMoeda.Size = new Size(54, 17);
+            lblMoeda.TabIndex = 14;
+            lblMoeda.Text = "Moeda:";
+            // 
+            // lblFusoHorario
+            // 
+            lblFusoHorario.AutoSize = true;
+            lblFusoHorario.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblFusoHorario.Location = new Point(14, 546);
+            lblFusoHorario.Name = "lblFusoHorario";
+            lblFusoHorario.Size = new Size(138, 17);
+            lblFusoHorario.TabIndex = 15;
+            lblFusoHorario.Text = "Fuso Horário (IANA):";
+            // 
+            // txbHost
+            // 
+            txbHost.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbHost.Location = new Point(183, 51);
+            txbHost.Name = "txbHost";
+            txbHost.ReadOnly = true;
+            txbHost.Size = new Size(220, 25);
+            txbHost.TabIndex = 30;
+            // 
+            // txbProvedor
+            // 
+            txbProvedor.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbProvedor.Location = new Point(183, 82);
+            txbProvedor.Name = "txbProvedor";
+            txbProvedor.ReadOnly = true;
+            txbProvedor.Size = new Size(220, 25);
+            txbProvedor.TabIndex = 31;
+            // 
+            // txbPais
+            // 
+            txbPais.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbPais.Location = new Point(183, 175);
+            txbPais.Name = "txbPais";
+            txbPais.ReadOnly = true;
+            txbPais.Size = new Size(219, 25);
+            txbPais.TabIndex = 32;
+            // 
+            // txbEstado
+            // 
+            txbEstado.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbEstado.Location = new Point(182, 206);
+            txbEstado.Name = "txbEstado";
+            txbEstado.ReadOnly = true;
+            txbEstado.Size = new Size(220, 25);
+            txbEstado.TabIndex = 33;
+            // 
+            // txbCidade
+            // 
+            txbCidade.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbCidade.Location = new Point(182, 237);
+            txbCidade.Name = "txbCidade";
+            txbCidade.ReadOnly = true;
+            txbCidade.Size = new Size(220, 25);
+            txbCidade.TabIndex = 34;
+            // 
+            // txbContinente
+            // 
+            txbContinente.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbContinente.Location = new Point(182, 268);
+            txbContinente.Name = "txbContinente";
+            txbContinente.ReadOnly = true;
+            txbContinente.Size = new Size(220, 25);
+            txbContinente.TabIndex = 35;
+            // 
+            // txbCodigoPostal
+            // 
+            txbCodigoPostal.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbCodigoPostal.Location = new Point(183, 352);
+            txbCodigoPostal.Name = "txbCodigoPostal";
+            txbCodigoPostal.ReadOnly = true;
+            txbCodigoPostal.Size = new Size(220, 25);
+            txbCodigoPostal.TabIndex = 36;
+            // 
+            // txbLatitude
+            // 
+            txbLatitude.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbLatitude.Location = new Point(183, 383);
+            txbLatitude.Name = "txbLatitude";
+            txbLatitude.ReadOnly = true;
+            txbLatitude.Size = new Size(220, 25);
+            txbLatitude.TabIndex = 37;
+            // 
+            // txbLongitude
+            // 
+            txbLongitude.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbLongitude.Location = new Point(183, 414);
+            txbLongitude.Name = "txbLongitude";
+            txbLongitude.ReadOnly = true;
+            txbLongitude.Size = new Size(220, 25);
+            txbLongitude.TabIndex = 38;
+            // 
+            // txbMoeda
+            // 
+            txbMoeda.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbMoeda.Location = new Point(183, 507);
+            txbMoeda.Name = "txbMoeda";
+            txbMoeda.ReadOnly = true;
+            txbMoeda.Size = new Size(219, 25);
+            txbMoeda.TabIndex = 40;
+            // 
+            // txbFusoHorario
+            // 
+            txbFusoHorario.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txbFusoHorario.Location = new Point(183, 538);
+            txbFusoHorario.Name = "txbFusoHorario";
+            txbFusoHorario.ReadOnly = true;
+            txbFusoHorario.Size = new Size(219, 25);
+            txbFusoHorario.TabIndex = 41;
+            // 
+            // lblOBS
+            // 
+            lblOBS.AutoSize = true;
+            lblOBS.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblOBS.Location = new Point(15, 336);
+            lblOBS.Name = "lblOBS";
+            lblOBS.Size = new Size(310, 13);
+            lblOBS.TabIndex = 42;
+            lblOBS.Text = "*Localização de coordenadas e código postal aproximadas.";
+            // 
+            // FrmResultados
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            ClientSize = new Size(415, 616);
+            Controls.Add(lblOBS);
+            Controls.Add(txbFusoHorario);
+            Controls.Add(txbMoeda);
+            Controls.Add(txbLongitude);
+            Controls.Add(txbLatitude);
+            Controls.Add(txbCodigoPostal);
+            Controls.Add(txbContinente);
+            Controls.Add(txbCidade);
+            Controls.Add(txbEstado);
+            Controls.Add(txbPais);
+            Controls.Add(txbProvedor);
+            Controls.Add(txbHost);
+            Controls.Add(lblFusoHorario);
+            Controls.Add(lblMoeda);
+            Controls.Add(lblLongitude);
+            Controls.Add(lblLatitude);
+            Controls.Add(lblCodigoPostal);
+            Controls.Add(lblContinente);
+            Controls.Add(lblCidade);
+            Controls.Add(lblEstado);
+            Controls.Add(lblPais);
+            Controls.Add(lblProvedor);
+            Controls.Add(lblHost);
+            Controls.Add(lblHorario);
+            Controls.Add(lblLocalizacao);
+            Controls.Add(lblConexao);
+            Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FrmResultados";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Resultados";
+            Load += FormResultados_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblConexao;
+        private Label lblLocalizacao;
+        private Label lblHorario;
+        private Label lblHost;
+        private Label lblProvedor;
+        private Label lblPais;
+        private Label lblEstado;
+        private Label lblCidade;
+        private Label lblContinente;
+        private Label lblCodigoPostal;
+        private Label lblLatitude;
+        private Label lblLongitude;
+        private Label lblMoeda;
+        private Label lblFusoHorario;
+        private TextBox txbHost;
+        private TextBox txbProvedor;
+        private TextBox txbPais;
+        private TextBox txbEstado;
+        private TextBox txbCidade;
+        private TextBox txbContinente;
+        private TextBox txbCodigoPostal;
+        private TextBox txbLatitude;
+        private TextBox txbLongitude;
+        private TextBox txbMoeda;
+        private TextBox txbFusoHorario;
+        private Label lblOBS;
+    }
+}
---
+++ projeto/geolocalizador-de-ip/FrmResultados.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace geolocalizador_de_ip
+{
+    public partial class FrmResultados : Form
+    {
+        public FrmResultados()
+        {
+            InitializeComponent();
+        }
+
+        public string ipString { get; set; } // O método 'get' recebe o conteúdo da textbox 'tbxIP' do form 'FormMenu' e o método 'set' envia para a string 'ipString';
+
+        private void FormResultados_Load(object sender, EventArgs e)
+        {
+            ActiveControl = null; // Define o foco para nenhum controle;
+
+            var ip = ipString; // A variável 'ip' recebe o conteúdo da string 'ipString';
+
+            var url = "http://ip-api.com/json/" + ip + "?fields=15738879&lang=pt-BR"; // Construindo a URL para fazer a solicitação ao serviço web com base no endereço IP fornecido;
+
+            using (HttpClient httpClient = new HttpClient()) // Criando uma nova instância da classe HttpClient que permite enviar solicitações HTTP para um servidor e receber respostas;
+            {
+                // Removendo o cabeçalho e definindo o formato do conteúdo da resposta para JSON:
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Passando o endereço da API para obter os detalhes de geolocalização:
+                httpClient.BaseAddress = new Uri(url);
+                HttpResponseMessage httpResponse = httpClient.GetAsync(url).GetAwaiter().GetResult();
+
+                
+                if (httpResponse.IsSuccessStatusCode) // Se a API for bem-sucedida e a resposta for recebida, os detalhes do local serão obtidos:
+                {
+                    // A resposta HTTP é lida e convertida para um objeto 'Ip_Api' (desserialização do conteúdo JSON da resposta HTTP):
+                    var geolocationInfo = httpResponse.Content.ReadAsAsync<Ip_Api>(new[] { new JsonMediaTypeFormatter() }).GetAwaiter().GetResult();
+
+                    if (geolocationInfo != null)
+                    {
+                        //txbIp.Text = geolocationInfo.ip;
+                        txbHost.Text = geolocationInfo.query;
+                        txbProvedor.Text = geolocationInfo.isp;
+
+                        txbPais.Text = geolocationInfo.country;
+                        txbEstado.Text = geolocationInfo.regionName + " (" + geolocationInfo.region + ")";
+                        txbCidade.Text = geolocationInfo.city;
+                        txbContinente.Text = geolocationInfo.continent + " (" + geolocationInfo.continentCode + ")";
+
+                        if (txbPais.Text == "Brasil")
+                        {
+                            txbCodigoPostal.Text = geolocationInfo.zip + "-000";
+                        }
+                        else 
+                        {
+                            txbCodigoPostal.Text = geolocationInfo.zip;
+                        }
+                        
+                        txbLatitude.Text = geolocationInfo.lat;
+                        txbLongitude.Text = geolocationInfo.lon;
+
+                        txbMoeda.Text = geolocationInfo.currency;
+                        txbFusoHorario.Text = geolocationInfo.timezone;
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    // Classe que corresponde à estrutura do JSON retornado pelo serviço web:
+    public class Ip_Api
+    {
+        public string query { get; set; }
+        public string continent { get; set; }
+        public string continentCode { get; set; }
+        public string country { get; set; }
+        public string countryCode { get; set; }
+        public string region { get; set; }
+        public string regionName { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
+        public string zip { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string timezone { get; set; }
+        public string currency { get; set; }
+        public string isp { get; set; }
+        public string org { get; set; }
+        public string asname { get; set; }
+    }
+}
